4c9b81401cdca5b6a44755b2afcd4fb44d7d69a8
tometjerry13000-arch <tometjerry13000@gmail.com>
2025-10-19 15:15:22 +0200

    Delete public directory

--- public/redirect.js
+++
@@ -1,9 +0,0 @@
-async function pollRedirect(){
-  const res = await fetch('/api/status');
-  const data = await res.json();
-  if(data.redirect){
-    window.location.href = data.redirect;
-  }
-  setTimeout(pollRedirect,1000);
-}
-pollRedirect();
